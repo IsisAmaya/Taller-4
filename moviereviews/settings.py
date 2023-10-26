fd824af5a93f7f0ece8c9bed9cc86a7dcf45052b
Isis Amaya <icamayaa@eafit.edu.com>
2023-10-26 10:33:17 -0500

    adicion ip dinamica

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '2hl5684eyz&6jd+3-rx=vg8enjb+d=f06p^lt^z%%nel1qds_q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['23.251.148.212']
 
 
 # Application definition
